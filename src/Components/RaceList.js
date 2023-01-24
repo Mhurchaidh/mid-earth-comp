53bc628db33fdc664cb32fd01ee910f9045a1461
mhurchaidh <bmurchie1997@gmail.com>
2023-01-23 22:35:07 -0800

    Added home components

---
+++ src/Components/RaceList.js
@@ -0,0 +1,24 @@
+function RaceList({characters}) {
+    return (
+        <div className="race-container">
+            <div id="men">
+                <h1>Men</h1>
+            </div>
+            <div id="elves">
+                <h1>Elves</h1>
+            </div>
+            <div id="maiar">
+                <h1>Maiar</h1>
+            </div>
+            <div id="dwarves">
+                <h1>Dwarves</h1>
+            </div>
+            <div id="orc-kind">
+                <h1>Orc-kind</h1>
+            </div>
+        </div>
+        
+    )
+}
+
+export default RaceList;
