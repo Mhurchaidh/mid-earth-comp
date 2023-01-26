7da132be7b6682b2fe404b136006347c03654998
mhurchaidh <bmurchie1997@gmail.com>
2023-01-25 22:18:46 -0800

    Refactored entire code to work with nested routes

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,17 +1,15 @@
-//import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Characters from "../db.json";
 import Header from "./Header";
 import RaceList from "./RaceList";
 import Form from "./Form";
-import Search from "./Search";
 import CharacterHolder from './CharacterHolder';
+import CharacterDetails from "./CharacterDetails";
 import { useState, useEffect } from "react";
 
 function App() {
 
   const [charactersArray, setCharactersArray] = useState(Characters.characters)
-  const [searchTerm, setSearchTerm] = useState("")
 
   useEffect(() => {
     fetch('http://localhost:3000/characters')
@@ -28,13 +26,11 @@ function App() {
     <div className="App">
       <Header />
       <Routes>
-        <Route exact path = '/' element={<RaceList characters={charactersArray} searchTerm={searchTerm}/>}/>
+        <Route path = '/' element={<RaceList characters={charactersArray}/>}/>
         <Route path='/addnew' element={<Form onFormSubmit={onFormSubmit}/>}/>
-        <Route path='/men' element={<CharacterHolder race={"Men"} characters={charactersArray}/>}/>
-        <Route path='/elves' element={<CharacterHolder race={"Elves"} characters={charactersArray}/>}/>
-        <Route path='/maiar' element={<CharacterHolder race={"Maiar"} characters={charactersArray}/>}/>
-        <Route path='/dwarves' element={<CharacterHolder race={"Dwarves"} characters={charactersArray}/>}/>
-        <Route path='/orc-kind' element={<CharacterHolder race={"Orcs"} characters={charactersArray}/>}/>
+        <Route path=':characterRace' element={<CharacterHolder characters={charactersArray}/>}>
+          <Route path=':characterName' element={<CharacterDetails characters={charactersArray}/>}/>
+        </Route>
       </Routes>
     </div>
   );
--- src/Components/CharacterDetails.js
+++ src/Components/CharacterDetails.js
@@ -1,4 +1,10 @@
-function CharacterDetails({character}) {
+import { useParams } from "react-router-dom";
+
+function CharacterDetails({characters}) {
+
+    const {characterName} = useParams();
+
+    const character = characters.find(char => char.name.toLowerCase() === characterName)
 
     return (
         <div className="character-details">
